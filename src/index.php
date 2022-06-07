ec1a79a44eaa6e95cf6845d9216d379b2dcad6c1
Romina Trazzi <romina.trazzi@posta.it>
2022-06-07 13:26:17 +0200

    Commit prova php pt 1

---
+++ src/index.php
@@ -0,0 +1,13 @@
+<?php 
+
+    $from = $_POST['from'];
+    $subject = $_POST['subject'];
+    $email = $_POST['email'];
+    $object = $_POST['object'];
+    $body = $_POST['body'];
+    
+    echo $from . " " . $subject;
+
+
+
+?>
