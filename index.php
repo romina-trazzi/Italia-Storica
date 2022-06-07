383562b42b49044a944d549fa1e8b5a495a81499
Romina Trazzi <romina.trazzi@posta.it>
2022-06-07 15:44:44 +0200

    Commit prova php pt 4 - Validazione email non funziona
    Con Mamp aperto funziona, i dati vengono passati

--- index.php
+++ index.php
@@ -1,28 +1,27 @@
-<<!DOCTYPE html>
+<!DOCTYPE html>
 
 <html>
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title> </title>
-        <meta name="description" content="">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="">
     </head>
+
+
     <body>
         
-        <?php 
+        <?php> 
 
             $from = $_POST['from'];  // proprietà name del campo del form nel file HTML
             $subject = $_POST['subject'];
             $email = $_POST['email'];
-            $object = $_POST['object'];
             $body = $_POST['body'];
             
-            echo $from . " " . $subject;
+            echo $from . " " . $subject . " " . $email . " " . $body;
             
         ?>
-        
+
+        <hr>
+
         <a href="index.html"> Torna alla home </a>
        
     </body>
